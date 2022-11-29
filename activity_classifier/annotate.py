a76008d4df45367f1498b155f172fc7e0c532851
arghasen10 <arghasen10@gmail.com>
2022-11-29 13:11:15 +0530

    macromodel

--- activity_classifier/annotate.py
+++ activity_classifier/annotate.py
@@ -1,20 +1,20 @@
 import csv
 from datetime import datetime, timedelta
 
-with open('aritra_activity1.csv', 'w') as file:
+with open('salma_micro_activity1.csv', 'w') as file:
     fieldnames = ['Datetime', 'Activity', 'User', 'Position', 'Orientation']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
 
-start_time = datetime.strptime("2022-11-26_20:29:04", "%Y-%m-%d_%H:%M:%S")
-end_time = datetime.strptime("2022-11-26_20:40:28", "%Y-%m-%d_%H:%M:%S")
+start_time = datetime.strptime("2022-11-24_00:00:15", "%Y-%m-%d_%H:%M:%S")
+end_time = datetime.strptime("2022-11-24_00:30:52", "%Y-%m-%d_%H:%M:%S")
 
 curr_time = start_time
 while curr_time < end_time:
-    with open('aritra_activity1.csv', 'a') as file:
+    with open('salma_micro_activity1.csv', 'a') as file:
         fieldnames = ['Datetime', 'Activity', 'User', 'Position', 'Orientation']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writerow({'Datetime': curr_time, 'Activity': 'Clapping', 'User': 'User-4', 'Position': 1, 'Orientation': 'Front'})
+        writer.writerow({'Datetime': curr_time, 'Activity': 'Clapping', 'User': 'User-3', 'Position': 1, 'Orientation': 'Front'})
 
     curr_time += timedelta(seconds=1)
 

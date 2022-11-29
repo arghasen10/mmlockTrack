3be32cdcec27fff10ee8a8c201f72d6a107f835b
arghasen10 <arghasen10@gmail.com>
2022-11-29 15:07:04 +0530

    spider_plot

--- spider_plot.py
+++ spider_plot.py
@@ -8,6 +8,8 @@ plt.rcParams["figure.figsize"] = (10, 7)
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 
+alpha = 0.25
+
 
 def _invert(x, limits):
     """inverts a value x on a scale from limits[0] to limits[1]"""
@@ -78,49 +80,49 @@ class ComplexRadar:
 
 # parameters
 variables = (
-    "             Area \n              Coverage \n             (max: 360\N{DEGREE SIGN})", "User orientation \n (max: 360\N{DEGREE SIGN})\n \n \n",
-    "Multi User \n \n ",
-    "Micro            \n    activities                ", "\n \n \n \n Macro     \n activities    \n",
-    "\n \n \n \n Monitoring paradigm \n(opportunistic: 1, continuous: 2)")
-ranges = [(360, 0), (360, 0), (10, 0),
-          (5, 0), (8, 0), (2, 0)]
+    "                       Area Coverage \n                        (max: 360\N{DEGREE SIGN})", "Multi User \n \n ",
+    "Micro                  \n    activities                      \n", "    User                 \n orientation              \n",
+    "\n \n \n \n Monitoring rate \n(opportunistic:1 , continuous:2)",
+    "\n \n \n \n Macro     \n activities    \n")
+ranges = [(360, 0), (10, 0),
+          (5, 0), (360, 0), (2, 0), (6, 0)]
 # data
-radhaar = (120, 120, 1,
-           0, 5, 1)
-mactivity = (120, 120, 1,
-             0, 5, 2)
-yu = (120, 120, 1,
-      0, 7, 1)
-mmpoint = (120, 120, 1,
-           4, 5, 1)
-wu = (120, 120, 1,
-      5, 7, 1)
-mmsense = (360, 360, 5,
-           2, 1, 2)
-mmgaitnet = (360, 360, 5,
-             0, 1, 1)
-ourmethod = (360, 360, 10,
-             5, 8, 2)
+radhaar = (110, 0.9,
+           0, 110, 1.1, 5.2 )
+mactivity = (120, 1,
+             0, 120, 2,4.8)
+# yu = (115, 115, 1,
+#       0, 7, 1)
+mmpoint = (120, 1.1,
+           4, 120, 1, 5)
+# wu = (125, 125, 1,
+#       5, 7, 1)
+mmsense = (340, 4.8,
+           2, 340, 1.9, 1 )
+mmgaitnet = (355, 5,
+             0, 355, 1, 1)
+ourmethod = (360, 10,
+             5, 360, 2, 6)
 
 # plotting
 fig1 = plt.figure(figsize=(10, 6))
 radar = ComplexRadar(fig1, variables, ranges)
-radar.plot(radhaar, "-", lw=2, color="b", alpha=0.9, marker = 'o', ms = 9, label="RadHAR [12]")
-radar.fill(radhaar, alpha=0.1)
-radar.plot(mactivity, "-", lw=2, color="r", alpha=0.9, marker = 's', ms = 9, label="m-Activity")
-radar.fill(mactivity, alpha=0.2)
-radar.plot(yu, "-", lw=2, color="m", alpha=0.9, marker = '+', ms = 9, label="DVCNN")
-radar.fill(yu, alpha=0.2)
-radar.plot(mmpoint, "-", lw=2, color="c", alpha=0.9, marker = 'x', ms = 9, label="MMPoint-GNN")
-radar.fill(mmpoint, alpha=0.1)
-radar.plot(wu, "-", lw=2, color="y", alpha=0.9, marker = 'h', ms = 9, label="3D Radar")
-radar.fill(wu, alpha=0.1)
-radar.plot(mmsense, "-", lw=2, color='mediumseagreen', alpha=0.9, marker = 'v', ms = 9, label="mmSense")
-radar.fill(mmsense, alpha=0.1)
-radar.plot(mmgaitnet, "-", lw=2, color='aquamarine', alpha=0.9, marker = 'D', ms = 9, label="mmGaitNet")
-radar.fill(mmgaitnet, alpha=0.1)
-radar.plot(ourmethod, "-", lw=2, color='0', alpha=0.9, marker = '*', ms = 9, label="MARS")
-radar.fill(ourmethod, alpha=0.1)
+radar.plot(radhaar, "-", lw=2, color="b", alpha=alpha, marker = 'o', ms = 9, label="RadHAR [12]")
+radar.fill(radhaar, alpha=alpha, color="b")
+radar.plot(mactivity, "-", lw=2, color="r", alpha=alpha, marker = 's', ms = 9, label="m-Activity []")
+radar.fill(mactivity, alpha=alpha, color="r")
+# radar.plot(yu, "-", lw=2, color="m", alpha=alpha, marker = '+', ms = 9, label="DVCNN")
+# # radar.fill(yu, alpha=alpha, color="m")
+# radar.plot(mmpoint, "-", lw=2, color="c", alpha=alpha, marker = 'x', ms = 9, label="MMPoint-GNN")
+# # radar.fill(mmpoint, alpha=alpha, color="c")
+# radar.plot(wu, "-", lw=2, color="y", alpha=alpha, marker = 'h', ms = 9, label="3D Radar")
+# # radar.fill(wu, alpha=alpha, color="y")
+radar.plot(mmsense, "-", lw=2, color='g', alpha=alpha, marker = 'v', ms = 9, label="mmSense []")
+radar.fill(mmsense, alpha=alpha, color='g')
+radar.plot(mmgaitnet, "-", lw=2, color='tab:orange', alpha=alpha, marker = 'D', ms = 9, label="mmGaitNet []")
+radar.fill(mmgaitnet, alpha=alpha, color='tab:orange')
+radar.plot(ourmethod, "-", lw=2, color='0', alpha=alpha, marker = '*', ms = 9, label="MARS []")
+radar.fill(ourmethod, alpha=alpha)
 radar.ax.legend(bbox_to_anchor=(1.1, 1.1), loc='upper left', borderaxespad=0)
 plt.savefig('sample_dimensions.pdf',bbox_inches='tight')
 plt.show()
